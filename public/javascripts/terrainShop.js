0a5ed4ff2f06859957cde555d3f6806ddb542eb4
ristes <ristes@github.com>
2014-04-27 01:38:16 +0200

    resolved conflict

--- public/javascripts/terrainShop.js
+++ public/javascripts/terrainShop.js
@@ -15,7 +15,6 @@ Crafty.c("TerrainFeature", {
 	data : function(data) {
 		var text = "";
 		for (var i = 0; i < data.length; i++) {
-<<<<<<< HEAD
 			text += data[i].category.name + ": " + data[i].value + "; ";
 		}
 		this.text(text);
@@ -54,54 +53,6 @@ Crafty.c("AnalyzeTerrainButton", {
 					self.modelData.analysis.features).attr({
 				xoffset : 10,
 				yoffset : 50
-=======
-			var btn = Crafty.e("2D, DOM, Text, Mouse").attr({
-				w : 400,
-				h : 20,
-				x : 150,
-				y : 120 + 30 * i
-			}).text(data[i].description).css({
-				"text-align" : "left",
-				"font-size" : "50px",
-				"padding-top" : "5px",
-				"padding-left" : "5px",
-				"background-color" : "green"
-			});
-			var analyze = Crafty.e("2D, DOM, Text, Mouse").attr({
-				w : 40,
-				h : 14,
-				x : 250,
-				y : 123 + 30 * i
-			}).text("analyze").css({
-				"text-align" : "center",
-				"font-size" : "50px",
-				"padding-top" : "5px",
-				"cursor" : "pointer",
-				"background-color" : "red"
-			}).bind("Click", function() {
-				var features = this.terrain.analysis.features;
-				for (var i = 0; i < features.length; i++) {
-					console.log(features[i]);
-				}
-				ModelStore.getFromService("/terrainshop/analyze?terrainId=" + this.terrain.analysis.entityId, function(dataTerrain) {
-					for (var j = 0; j < dataTerrain.length; j++) {
-						var analyseFeatures = Crafty.e("2D, DOM, Text, Mouse").attr({
-							w : 400,
-							h : 20,
-							x : 600,
-							y : 120 + 30 * j
-						}).text(dataTerrain[j].analysis + dataTerrain[j].value).css({
-							"text-align" : "left",
-							"font-size" : "50px",
-							"padding-top" : "5px",
-							"padding-left" : "5px",
-							"background-color" : "green"
-						}).bind("Click", function() {
-							console.log(this);
-						});
-					}
-				});
->>>>>>> 99b6bf559b7c135d50e19057f876daebe857a94d
 			});
 			self.container.addCmp(cmp);
 			self.container.attr("h", self.container.h + cmp.h + 16);
@@ -203,7 +154,6 @@ Crafty.scene("terrainShop", function() {
 		"text-align" : "center",
 		"font-size" : "50px"
 	});
-<<<<<<< HEAD
 
 	Crafty.e("List").itemFn(function(data, x, y) {
 		var item = Crafty.e("TerrainText").data(data).attr({
@@ -217,6 +167,3 @@ Crafty.scene("terrainShop", function() {
 	});
 
 });
-=======
-});
->>>>>>> 99b6bf559b7c135d50e19057f876daebe857a94d
