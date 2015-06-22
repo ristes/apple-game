321dc74dc2ee5bf01259fb175f7414dfbbb2f381
kokimishev <kokimisev@gmail.com>
2015-06-22 10:40:58 +0200

    bug resolved

--- app/service/impl/LearnStateEventServiceImpl.java
+++ app/service/impl/LearnStateEventServiceImpl.java
@@ -37,7 +37,7 @@ public class LearnStateEventServiceImpl implements LearnStateEventService {
 	public List<LearnStateEvents> avail(Farmer farmer) {
 
 		List<LearnStateEvents> avail = new ArrayList<LearnStateEvents>();
-		String sqlString = "SELECT * FROM learn_state_events WHERE id NOT IN (SELECT learn_state_events.id FROM applegame.learn_state_events inner join learn_state_event_executed on learn_state_events.id=learn_state_event_executed.event_id where farmer_id=:id)";
+		String sqlString = "SELECT * FROM learn_state_events WHERE id NOT IN (SELECT learn_state_events.id FROM learn_state_events inner join learn_state_event_executed on learn_state_events.id=learn_state_event_executed.event_id where farmer_id=:id)";
 		Query query = JPA.em().createNativeQuery(sqlString);
 		query.setParameter("id", farmer.id);
 		List<Object[]> objRes = query.getResultList();
