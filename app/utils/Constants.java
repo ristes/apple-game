f7d8e64cdd5705b2f416a53e3217e44b43d995a8
kokimishev <kokimisev@gmail.com>
2015-09-21 15:37:47 +0200

    fb key

--- app/utils/Constants.java
+++ app/utils/Constants.java
@@ -2,8 +2,8 @@ package utils;
 
 public class Constants {
 
-	public static final String API_KEY = "671566419604666";
-	public static final String APP_SECRET = "69dc2db9bb3deaa66785c9032fca8181";
+	public static final String API_KEY = "761102634001845";
+	public static final String APP_SECRET = "40165a03cf93f63c4f12df98b945715c";
 	
 	public static final Integer money_obtained_by_share = 2000;
 	
