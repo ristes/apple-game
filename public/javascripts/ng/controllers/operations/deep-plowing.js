32ea475837d0350271026ebfb59c2b252fc795cb
kokimishev <kokimisev@gmail.com>
2015-09-25 11:21:58 +0200

    css

--- public/javascripts/ng/controllers/operations/deep-plowing.js
+++ public/javascripts/ng/controllers/operations/deep-plowing.js
@@ -1,17 +1,20 @@
 Game.controller('DeepPlowingController', [
     '$scope',
     'Plowing',
-    function($scope, Plowing) {
+    'Toaster',
+    function($scope, Plowing, Toaster) {
 
       var unreg = $scope.$root.$on('operation-deep-plowing',
               function(_s, oper) {
 
-                $scope.$root.$emit('show-progress-global', {
-                  title: 'progress_deep_plowing',
-                  duration: 10,
-                  actionToShare:'DeepPlowing'
-                });
+//                $scope.$root.$emit('show-progress-global', {
+//                  title: 'progress_deep_plowing',
+//                  duration: 10,
+//                  actionToShare:'DeepPlowing'
+//                });
                 Plowing.deepPlowing();
+                Toaster.success("Plowing is done! Great job!");
+                
               });
 
       $scope.$on("$destroy", function() {
